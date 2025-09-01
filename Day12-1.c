0ec271fe649c3c8aadfd150dd69406aeb6de8efd
Bhoomika Arora <arorabhoomika34@gmail.com>
2025-09-01 22:01:03 +0530

    Add files via upload

---
+++ Day12-1.c
@@ -0,0 +1,28 @@
+//Q23: Write a program to calculate a library fine based on late days.//
+#include <stdio.h>
+int main(){
+    int days;
+    printf("Enter the number of late days: ");
+    scanf("%d",&days);
+    if(days<=0){
+        printf("No fine");
+    }
+    else if(days<=5){
+         int Fine=days*1;
+        printf("Total fine is '%d'",Fine);
+    }
+    else if(days<=10){
+        int Fine=days*2;
+        printf("Total fine is '%d'",Fine);
+    }
+    else if(days<=15){
+        int Fine=days*3;
+        printf("Total fine is '%d'",Fine);
+    } 
+    else{
+        printf("Membership Cancelled");
+    }  
+    return 0;
+}
+
+
