d537fdb0156eac209e70c47cc0241f43d1694fe7
Bhoomika Arora <arorabhoomika34@gmail.com>
2025-09-08 22:59:43 +0530

    Add files via upload

---
+++ Day18-2.c
@@ -0,0 +1,16 @@
+//Q36: Write a program to find the HCF (GCD) of two numbers.//
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    printf("%d\n", a);
+
+    return 0;
+}
+
