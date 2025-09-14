0f6fbbd48ee950cd9f6bc1bc09eed7ff810222ae
Bhoomika Arora <arorabhoomika34@gmail.com>
2025-09-15 02:52:51 +0530

    Update Day21-1.c

--- Day21-1.c
+++ Day21-1.c
@@ -1,32 +1,38 @@
-//Q41: Write a program to swap the first and last digit of a number.//
 #include <stdio.h>
 #include <math.h>
 
-int swapFirstLast(int num) {
-    int n = num;
-    int firstDigit, lastDigit;
-    int digits = 0;
+int main() {
+    int num, originalNum;
+    int firstDigit, lastDigit, digits = 0;
+    
+    // Input the number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    originalNum = num;
+    lastDigit = num % 10;
 
-    lastDigit = n % 10;
-    while (n != 0) {
-        firstDigit = n;
-        n = n / 10;
+    // Count digits and extract first digit
+    int temp = num;
+    while (temp >= 10) {
+        temp /= 10;
         digits++;
     }
-    if (digits == 1) return num;
-    int power = pow(10, digits - 1);
-    int middle = (num % power) / 10;
-    int newNum = lastDigit * power + middle * 10 + firstDigit;
+    firstDigit = temp;
 
-    return newNum;
-}
+    // If number has only one digit, or first == last, no change
+    if (originalNum < 10 || firstDigit == lastDigit) {
+        printf("Swapped number: %d\n", originalNum);
+        return 0;
+    }
 
-int main() {
-    int num;
-    scanf("%d", &num);
+    // Remove first digit
+    int middlePart = (originalNum % (int)pow(10, digits)) / 10;
 
-    int result = swapFirstLast(num);
-    printf("%d\n", result);
+    // Reconstruct number with swapped digits
+    int swapped = lastDigit * (int)pow(10, digits) + middlePart * 10 + firstDigit;
+
+    printf("Swapped number: %d\n", swapped);
+
     return 0;
-}    
-
+}
