1d484a98f164e7c9214b6195b02de2527adde50a
Bhoomika Arora <arorabhoomika34@gmail.com>
2025-09-16 23:05:25 +0530

    Add files via upload

---
+++ Day27-2.c
@@ -0,0 +1,30 @@
+/*Q54: Write a program to print the following pattern:
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+    */
+#include <stdio.h>
+int main(){
+   for(int i=1;i<=4;i++){
+      for(int j=1;j<=4-i;j++)
+      printf(" ");
+      for(int j=1;j<=2*i-1;j++)
+      printf("*");
+   }
+   for(i = 3; i >= 1; i--) {
+        for(j = 1; j <= 4-i; j++)
+            printf(" ");
+        for(j = 1; j <= (2 * i - 1); j++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
