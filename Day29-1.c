5601110494c7ae289d1531ed0ff36ac4e1b01d2a
Bhoomika Arora <arorabhoomika34@gmail.com>
2025-09-18 22:11:30 +0530

    Add files via upload

---
+++ Day29-1.c
@@ -0,0 +1,13 @@
+//Q57: Find the sum of array element//
+#include <stdio.h>
+int main(){
+    int n,i,sum=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    printf("%d\n",sum);
+    return 0;
+}
