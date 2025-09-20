92fc737cc599d6c0adc78f4a6048bda1463c6104
Bhoomika Arora <arorabhoomika34@gmail.com>
2025-09-20 16:46:41 +0530

    Add files via upload

---
+++ Day31-1.c
@@ -0,0 +1,25 @@
+//Q61: Search for an element in an array using linear search.//
+#include <stdio.h>
+int main(){
+    int n,i,key,pos=-1;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&key);
+    for(i=0;i<n;i++){
+        if(arr[i]==key){
+            pos=i;
+            break;
+        }
+    }
+    if(pos==-1){
+        printf("-1\n");
+    }
+    else{
+        printf("Found at index %d\n",pos);
+    }
+    return 0;
+}
+
