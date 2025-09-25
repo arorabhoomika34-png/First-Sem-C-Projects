08c530850dc62b7807be5a6df256754fd2972706
Bhoomika Arora <arorabhoomika34@gmail.com>
2025-09-25 21:43:03 +0530

    Update Day36-1.c

--- Day36-1.c
+++ Day36-1.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 int main(){
     int rows,cols;
+    printf("Enter the no. of rows and cols");
     scanf("%d %d",&rows,&cols);
     int matrix[rows][cols];
+    printf("Enter the elements of matrix");
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
             scanf("%d",&matrix[i][j]);
@@ -16,4 +18,5 @@ int main(){
         printf("\n");
     }
     return 0;
+
 }
