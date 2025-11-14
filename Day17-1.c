fe2a6311ddcfb595273dfef59d2f4156c9d22111
Bhoomika Arora <arorabhoomika34@gmail.com>
2025-11-14 22:24:54 +0530

    Add files via upload

---
+++ Day17-1.c
@@ -0,0 +1,20 @@
+//Q33: Write a program to check if a number is an Armstrong number.//
+#include <stdio.h>
+int main(){
+    int n,originalnum,result=0;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    originalnum=n;
+    while(originalnum!=0){
+       int r=originalnum%10; 
+       result+=r*r*r;
+       originalnum=originalnum/10;
+    }
+    if(result==n){
+        printf("Number is armstrong");
+    }
+    else{
+        printf("Number is not armstrong");
+    }
+    return 0;
+}
