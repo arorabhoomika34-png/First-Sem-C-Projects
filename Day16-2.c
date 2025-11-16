f9bcb9ef66a981e9e8c7ff354e52fe6fd33e09ba
Bhoomika Arora <arorabhoomika34@gmail.com>
2025-11-16 21:57:59 +0530

    Add files via upload

---
+++ Day16-2.c
@@ -0,0 +1,20 @@
+//Q31: Write a program to take a number as input and print its equivalent binary representation.//
+#include <stdio.h>
+int main(){
+    int n,originalnum,result=0;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    originalnum=n;
+    while(originalnum!=0){
+       int r=originalnum%10; 
+       result+=r*r*r;
+       originalnum=originalnum/10;
+    }
+    if(result==n){
+        printf("Number is armstrong");
+    }
+    else{
+        printf("Number is not armstrong");
+    }
+    return 0;
+}
